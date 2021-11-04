ba6bc24dd3f393fbf5fe563f5b3dfd2a87a50e01
yuanxinxin <yuanxinxin@111.com.cn>
2021-11-04 14:27:06 +0800

    demo

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ app.get('*', function(req, res) {
                 }, 5000);
             })
             break;
-        case '/2.js':
-            //延迟 10s
-            fs.readFile("./2.js", "utf-8", function(err, data) {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                setTimeout(function() {
-                    res.write(data);
-                    res.end();
-                }, 10000);
-            })
-            break;
         case '/1.css':
             //延迟 15s
             fs.readFile("./1.css", "utf-8", function(err, data) {
@@ -39,25 +29,6 @@ app.get('*', function(req, res) {
                     res.write(data);
                     res.end();
                 }, 15000);
-            })
-            break;
-        case '/2.css':
-            //延迟 20s
-            fs.readFile("./2.css", "utf-8", function(err, data) {
-                res.writeHead(200, { 'Content-Type': 'text/css' });
-                setTimeout(function() {
-                    res.write(data);
-                    res.end();
-                }, 20000);
-            })
-            break;
-        case '/3.css':
-            fs.readFile("./3.css", "utf-8", function(err, data) {
-                res.writeHead(200, { 'Content-Type': 'text/css' });
-                setTimeout(function() {
-                    res.write(data);
-                    res.end();
-                }, 20000);
             })
             break;
     }
